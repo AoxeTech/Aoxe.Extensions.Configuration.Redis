9b90bbb4fe32b0cff152b587586810ec9fa92760
RomanDu <aeondxf@live.com>
2024-09-09 10:23:38 +0800

    Dev

--- src/Aoxe.Extensions.Configuration.StackExchangeRedis/RedisConfigurationExtensions.cs
+++ src/Aoxe.Extensions.Configuration.StackExchangeRedis/RedisConfigurationExtensions.cs
@@ -4,8 +4,15 @@ public static class RedisConfigurationExtensions
 {
     public static IConfigurationBuilder AddRedis(
         this IConfigurationBuilder builder,
-        RedisClientOptions redisClientOptions,
+        Func<RedisClientOptions> optionsFactory,
         string key,
         IFlattener? flattener = null
-    ) => builder.Add(new RedisConfigurationSource(redisClientOptions, key, flattener));
+    ) => builder.Add(new RedisConfigurationSource(optionsFactory, key, flattener));
+
+    public static IConfigurationBuilder AddRedis(
+        this IConfigurationBuilder builder,
+        RedisClientOptions options,
+        string key,
+        IFlattener? flattener = null
+    ) => builder.Add(new RedisConfigurationSource(options, key, flattener));
 }
--- src/Aoxe.Extensions.Configuration.StackExchangeRedis/RedisConfigurationSource.cs
+++ src/Aoxe.Extensions.Configuration.StackExchangeRedis/RedisConfigurationSource.cs
@@ -9,6 +9,13 @@ public class RedisConfigurationSource(
     public RedisClientOptions RedisClientOptions { get; } = redisClientOptions;
     public string Key { get; } = key;
 
+    public RedisConfigurationSource(
+        Func<RedisClientOptions> optionsFactory,
+        string key,
+        IFlattener? flattener = null
+    )
+        : this(optionsFactory(), key, flattener) { }
+
     public IConfigurationProvider Build(IConfigurationBuilder builder) =>
         new RedisConfigurationProvider(this, new RedisDatabaseFactory(this), flattener);
 }
